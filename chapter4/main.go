1dfa385c4e7b75ee89a1ea3e68a437bbbae890d3
maxchichar <maxwellchibuezecharles@gmail.com>
2026-03-25 10:33:56 +0100

    chapter4 incomplete

---
+++ chapter4/main.go
@@ -0,0 +1,31 @@
+//Control Structures
+
+package main
+
+import(
+	"fmt"
+)
+
+func main()  {
+	//For statement
+
+	/*
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
+	*/
+
+	//Or
+
+	/*
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+	*/ 
+
+	//If satement
+	
+
+}
